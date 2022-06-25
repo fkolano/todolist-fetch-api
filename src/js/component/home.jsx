0ac86a9dec34f628ce9f4068f7dbd4e1406c6c60
fkolano <fkolano@gmail.com>
2022-06-25 15:58:45 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const Home = () => {
+  const [todoList, setToDoList] = useState(["Do Homework",
+  "Do Laundry",
+  "Walk the Dog"]);
+  const newToDoList = todoList.map((item, i) => {
+    return (
+      <div key={i}>
+        <li>
+          {item} <button onClick={() => deleteToDoList(i)}>x</button>
+        </li>
+      </div>
+    );
+  });
+  const deleteToDoList = (index) => {
+    const newArray = todoList.filter((item, i) => i != index);
+    setToDoList(newArray);
+  };
+  const newToDo = (onKeyDownEvent) => {
+    if (onKeyDownEvent.keyCode === 13) {
+      let click = onKeyDownEvent.target.value;
+      const todo = [...todoList, click];
+      setToDoList(todo);
+      onKeyDownEvent.target.value = "";
+      
+    }
+  };
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container justify-content-center">
+      <h1>todos</h1>
+      <input
+        placeholder="What needs to be done"
+        onKeyDown={newToDo}
+        type="text"
+      ></input>
+      <ul>{newToDoList}</ul>
+    </div>
+  );
 };
-
-export default Home;
